d8d522556551bc925760653c1b93daf041aae1e8
huajian <287643967@qq.com>
2020-05-11 08:57:03 +0800

    添加搜索框

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 import 'app.dart';
 import 'global.dart';
 
-void main() => Global.init().then((e) {
-      runApp(MyApp());
-    });
+void main() => Global.init().then((e) => runApp(MyApp()));
